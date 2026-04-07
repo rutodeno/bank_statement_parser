39bffd7bfb3b46be5756d110d2e46f510ab9e52a
rutodeno <rutodeno@gmail.com>
2026-04-06 22:53:39 -0400

    adding detection for debit and credit statements

---
+++ src/bank_statement_parser/banks/amex/amex_credit.py
@@ -0,0 +1,9 @@
+import re
+from datetime import datetime
+from typing import List
+from interfaces.base_parser import BankStatementParser
+from models.transaction import Transaction
+
+
+class AmexCreditParser(BankStatementParser):
+    pass
